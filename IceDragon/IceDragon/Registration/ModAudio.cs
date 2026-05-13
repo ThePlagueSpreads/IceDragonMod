670b18b31295aa21018983e65b4d79cc2a643ba8
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-05-12 21:50:56 -0400

    Register biome music

--- IceDragon/IceDragon/Registration/ModAudio.cs
+++ IceDragon/IceDragon/Registration/ModAudio.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using FMOD;
+using Nautilus.Extensions;
 using Nautilus.FMod;
 using Nautilus.Handlers;
 using Nautilus.Utility;
@@ -10,6 +12,8 @@ namespace IceDragon.Registration;
 public static class ModAudio
 {
     public static FMODAsset Roar { get; } = AudioUtils.GetFmodAsset("PodshellMusic");
+    
+    public static FMODAsset BiomeMusic { get; } = AudioUtils.GetFmodAsset("IceSpikesMusic");
 
     public static void RegisterAudio()
     {
@@ -20,6 +24,8 @@ public static class ModAudio
             "icedragon_roar4",
             "icedragon_roar5"
         ]);
+        
+        RegisterMusic(BiomeMusic, "IceSpikes");
     }
 
     private static void RegisterDragonSound(FMODAsset asset, string clipName, float minDistance, float maxDistance, string bus = AudioUtils.BusPaths.UnderwaterCreatures)
@@ -45,5 +51,18 @@ public static class ModAudio
         var multiSoundsEvent = new FModMultiSounds(sounds.ToArray(), AudioUtils.BusPaths.UnderwaterCreatures, true);
         
         CustomSoundHandler.RegisterCustomSound(asset.path, multiSoundsEvent);
+    }
+    
+    private static void RegisterMusic(FMODAsset asset, string clipName, float fadeOutDuration = 3f,
+        bool useSoundEffectsBus = false, bool looping = false)
+    {
+        var mode = AudioUtils.StandardSoundModes_2D;
+        if (looping)
+            mode |= MODE.LOOP_NORMAL;
+        var sound = AudioUtils.CreateSound(ModRegistration.Assets.LoadAsset<AudioClip>(clipName), mode);
+        if (fadeOutDuration > Mathf.Epsilon)
+            sound.AddFadeOut(fadeOutDuration);
+        CustomSoundHandler.RegisterCustomSound(asset.id, sound,
+            useSoundEffectsBus ? "bus:/master/SFX_for_pause/PDA_pause/all" : AudioUtils.BusPaths.Music);
     }
 }
