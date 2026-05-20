837dab43b94de63a16d04fbc79b5565530b5e30d
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-05-20 02:05:04 -0400

    Fix NRE

--- IceDragon/IceDragon/MonoBehaviours/IceProjectile.cs
+++ IceDragon/IceDragon/MonoBehaviours/IceProjectile.cs
@@ -30,7 +30,7 @@ public class IceProjectile : MonoBehaviour, IManagedUpdateBehaviour
     {
         var lm = collision.gameObject.GetComponentInParent<LiveMixin>();
         
-        if (lm.gameObject.GetComponent<IceDragonRangedAttack>() != null)
+        if (lm != null && lm.gameObject.GetComponent<IceDragonRangedAttack>() != null)
             return;
         
         bool shatter = collision.impulse.magnitude > 4;
